ac6f2e9a79f0e9b94e5c3f0cd14760228daa7b87
Andrgoit <93829515+Andrgoit@users.noreply.github.com>
2022-10-18 11:26:16 +0000

    Deploying to gh-pages from @ Andrgoit/goit-react-hw-05-movies@40366b5aba9bfabfd23b363bcb5b4ae118f4eddf 🚀

--- static/js/186.63aef70a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[186],{186:function(e,t,i){i.r(t),i.d(t,{default:function(){return r}});i(791);var n=i(184);function r(){return(0,n.jsx)("div",{children:"Reviews"})}}}]);
-//# sourceMappingURL=186.63aef70a.chunk.js.map
